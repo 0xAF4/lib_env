fabef85409c004c32ddd5928856b87a027c3e0b7
0xAF4 <karim.badalov1612@gmail.com>
2023-11-11 16:32:37 +0600

    gg

--- index.go
+++ index.go
@@ -1,9 +1,45 @@
 package lib_env
 
 import (
-	"fmt"
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
 )
 
-func HelloWorld() {
-	fmt.Println("f*ck")
+type Config struct {
+	App struct {
+		Name    string `env:"APP_NAME"`
+		Version string `env:"APP_VERSION"`
+	}
+
+	Http struct {
+		Port  string `env:"HTTP_PORT"`
+		Group string `env:"HTTP_PORT"`
+	}
+
+	Jwt struct {
+		Key       string `env:"JWT_KEY"`
+		Algorithm string `env:"JWT_ALGORITHM"`
+		LiveTime  string `env:"JWT_LIVE_TIME"`
+	}
+
+	Telegram struct {
+		Token  string `env:"TELEGRAM_TOKEN"`
+		ChatID string `env:"TELEGRAM_CHAT_ID"`
+		Users  string `env:"TELEGRAM_USERS"`
+	}
+
+	Mail struct {
+		Server     string `env:"MAIL_SERVER"`
+		Port       string `env:"MAIL_PORT"`
+		SenderMail string `env:"MAIL_SENDER_MAIL"`
+	}
+}
+
+var (
+	Con *Config
+)
+
+func init() {
+	godotenv.Load()
+	env.Parse(&Con)
 }
